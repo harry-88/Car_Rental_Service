0d9d6bc8b63121083f7d7b6ca8a3db33f484e154
harry-88 <harisbro88@gmail.com>
2021-04-08 08:17:43 -0700

    4

--- app/src/main/java/com/atmostsoft/quiz1/StartScreen.java
+++ app/src/main/java/com/atmostsoft/quiz1/StartScreen.java
@@ -3,12 +3,22 @@ package com.atmostsoft.quiz1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class StartScreen extends AppCompatActivity {
 
+    Button btnAddDriver,btnLocation,btnDriverNumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+    }
+
+    public void init()
+    {
+        btnAddDriver = findViewById(R.id.btnAddDriver);
+        btnLocation = findViewById(R.id.btnLocation);
+        btnDriverNumber = findViewById(R.id.btnDriverNumber);
     }
 }
