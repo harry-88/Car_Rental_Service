24628ec1b10056055b0072ec1c118cf4f063d9a5
harry-88 <harisbro88@gmail.com>
2021-04-08 00:19:18 -0700

    Signup screen use to add manager info

--- app/src/main/java/com/atmostsoft/quiz1/MainActivity.java
+++ app/src/main/java/com/atmostsoft/quiz1/MainActivity.java
@@ -2,17 +2,44 @@ package com.atmostsoft.quiz1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etNumber,etPassword;
-    Button btnEnter;
+    Button btnEnter,btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etNumber.getText().toString().isEmpty())
+                    etNumber.setError("number is required");
+                if (etPassword.getText().toString().isEmpty())
+                    etPassword.setError("password is required");
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,com.atmostsoft.quiz1.SignUp.class);
+//                startActivityForResult(intent,ACTIVITY_SI);
+            }
+        });
+    }
+    public void init()
+    {
+        etNumber = findViewById(R.id.number);
+        etPassword = findViewById(R.id.password);
+        btnEnter = findViewById(R.id.btnEnter);
+        btnSignUp = findViewById(R.id.signUp);
     }
 }
